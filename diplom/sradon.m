d4dd1415c3fd069994759bd705e6d55bc9b1edf9
medvi <tc-sport@yandex.ru>
2016-03-22 15:34:52 +0300

    sradon done, isradon not working=(

---
+++ diplom/sradon.m
@@ -0,0 +1,28 @@
+function [ sradon_result ] = sradon(f, point_coords, radius)
+%SRADON Сферическое преобразование Радона
+%   Функция sradon возвращает значение сферического преобразования Радона
+%   функции f по окружности с центром в точке point_coords и радиусом
+%   radius.
+
+    syms t % объявление символьной переменной t
+    x = sym('x', [1 2]); % объявление вектора символьных переменных x1 и x2
+    
+    % параметризация единичной окружности
+    r(t) = [cos(t), sin(t)];
+    
+    % функция f от нужного аргумента
+    subs_f = subs(f, x, point_coords + radius * r);
+    
+    % подынтегральная функция; нужно дополнительно домножить 
+    % на корень из суммы квадратов x'(t) и y'(t)
+    integrand_f = sum((subs_f.*transpose(sqrt(sum(diff(r).^2)))));
+    %integrand_f
+    
+    % интеграл от подынтегральной функции по t от 0 до 2*pi
+    if radius == 0
+        sradon_result = subs(f, x, point_coords);
+    else
+        sradon_result = int(integrand_f, t, 0, 2*pi) / (pi * radius * radius);
+    end
+end
+
