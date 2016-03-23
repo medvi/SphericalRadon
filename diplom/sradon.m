d33d0bfd4d859bdce45fc82abbb216cb6c2f440c
medvi <tc-sport@yandex.ru>
2016-03-23 18:09:56 +0300

    sradon optimized, but needed checking

--- diplom/sradon.m
+++ diplom/sradon.m
@@ -3,26 +3,28 @@ function [ sradon_result ] = sradon(f, point_coords, radius)
 %   ‘ункци€ sradon возвращает значение сферического преобразовани€ –адона
 %   функции f по окружности с центром в точке point_coords и радиусом
 %   radius.
-
+% 
     syms t % объ€вление символьной переменной t
-    x = sym('x', [1 2]); % объ€вление вектора символьных переменных x1 и x2
-    
+%    x = sym('x', [1 2]); % объ€вление вектора символьных переменных x1 и x2
+%     
     % параметризаци€ единичной окружности
     r(t) = [cos(t), sin(t)];
-    
+%     
     % функци€ f от нужного аргумента
-    subs_f = subs(f, x, point_coords + radius * r);
-    
+    %subs_f = subs(f, x, point_coords + radius * r);
+    integrand(t) = f(point_coords(1)+radius*cos(t), point_coords(2)+radius*sin(t));
     % подынтегральна€ функци€; нужно дополнительно домножить 
     % на корень из суммы квадратов x'(t) и y'(t)
-    integrand_f = sum((subs_f.*transpose(sqrt(sum(diff(r).^2)))));
+    %integrand_f = sum((subs_f.*transpose(sqrt(sum(diff(r).^2)))));
     %integrand_f
     
     % интеграл от подынтегральной функции по t от 0 до 2*pi
     if radius == 0
-        sradon_result = subs(f, x, point_coords);
+        sradon_result = double(f(point_coords(1), point_coords(2)));
     else
-        sradon_result = int(integrand_f, t, 0, 2*pi) / (pi * radius * radius);
+        %sradon_result = int(integrand_f, t, 0, 2*pi) / (pi * radius * radius);
+        n = 100;
+        sradon_result = double(simpsons(integrand, 0, 2*pi, n));
     end
 end
 
